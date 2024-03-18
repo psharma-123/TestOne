851fe87b55054d4f1c495cea3b1617e0c31f468b
Varinder jassal <varinderjassal@varinders-mbp.lan>
2024-03-18 19:13:23 -0400

    final commit

--- src/main/java/FirstTest/Chrome.java
+++ src/main/java/FirstTest/Chrome.java
@@ -16,30 +16,18 @@ public class Chrome {
         driver.manage().window().maximize();
         UserInformation object1 = new UserInformation("Albert", "Toronto,Ontario", "Vancouver,British Colombia", "albert123@gmail.com" );
         UserInformation object2 = new UserInformation("Marie","NewYork, US","Delhi,India","marie1112gmail.com");
-        displayUserInfoById(driver, object1);
-//        displayInfoByCssSelelctor(driver,object2);
-
-
+        UserInformation object3 = new UserInformation("Tony","24 Greenwood Drive,Brampton","32 President Drive Abbotsford","tonystark2@gmail.com");
+//      displayUserInfoById(driver, object1);
+//      displayInfoByCssSelector(driver,object2);
+//        displayInfoByXPath(driver,object3);
 
 //        driver.findElement(By.className(" mr-sm-2 form-control")).sendKeys("Albert");
 //        driver.findElement(By.className("form-control")).sendKeys("Toronto,Ontario");
-
-
-
-
-//        driver.findElement(By.xpath("//*[@id=\"userName\"]")).sendKeys("Albert");
-//        driver.findElement(By.xpath("//*[@id=\"userEmail\"]")).sendKeys("albert123@gmail.com");
-//        driver.findElement(By.xpath("//*[@id=\"currentAddress\"]")).sendKeys("Toronto, Ontario");
-//        driver.findElement(By.xpath("//*[@id=\"permanentAddress\"]")).sendKeys("Vancouver,British Colombia");
-//
-
 //        driver.findElement(By.tagName("input")).sendKeys("Albert");
 //        driver.findElement(By.tagName("textarea")).sendKeys("Toronto,Ontario");
-
     }
 
     public static void displayUserInfoById(WebDriver driver, UserInformation object1) {
-
         driver.findElement(By.id("userName")).sendKeys(object1.getName());
         driver.findElement(By.id("userEmail")).sendKeys(object1.getEmail());
         driver.findElement(By.id("currentAddress")).sendKeys(object1.getCurrentAddress());
@@ -47,11 +35,19 @@ public class Chrome {
         driver.findElement(By.id("submit")).click();
     }
 
-    public static void displayInfoByCssSelelctor(WebDriver driver, UserInformation object2) {
+    public static void displayInfoByCssSelector(WebDriver driver, UserInformation object2) {
         driver.findElement(By.cssSelector("#userName")).sendKeys(object2.getName());
         driver.findElement(By.cssSelector("#userEmail")).sendKeys(object2.getEmail());
         driver.findElement(By.cssSelector("#currentAddress")).sendKeys(object2.getCurrentAddress());
         driver.findElement(By.cssSelector("#permanentAddress")).sendKeys(object2.getPermanentAddress());
         driver.findElement(By.cssSelector("#submit")).click();
     }
+    public static void displayInfoByXPath(WebDriver driver,UserInformation object3) {
+        driver.findElement(By.xpath("//*[@id=\"userName\"]")).sendKeys(object3.getName());
+        driver.findElement(By.xpath("//*[@id=\"userEmail\"]")).sendKeys(object3.getEmail());
+        driver.findElement(By.xpath("//*[@id=\"currentAddress\"]")).sendKeys(object3.getCurrentAddress());
+        driver.findElement(By.xpath("//*[@id=\"permanentAddress\"]")).sendKeys(object3.getPermanentAddress());
+        driver.findElement(By.xpath("//*[@id=\"submit\"]")).click();
+    }
+
 }
